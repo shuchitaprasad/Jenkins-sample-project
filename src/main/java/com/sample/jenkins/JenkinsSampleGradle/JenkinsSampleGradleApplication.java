a4d83cbde9b1f4cafb003873756c667125394473
Shuchita Prasad <shuchita.prasad@mastercard.com>
2018-04-30 16:20:34 +0530

    sample gradle jenkins project

---
+++ src/main/java/com/sample/jenkins/JenkinsSampleGradle/JenkinsSampleGradleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.jenkins.JenkinsSampleGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSampleGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSampleGradleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sample/jenkins/JenkinsSampleGradle/JenkinsSampleGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.jenkins.JenkinsSampleGradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsSampleGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
